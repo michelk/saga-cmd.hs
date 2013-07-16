8413e14679e8242ffe57beb986ad70c4f6839077
Michel Kuhlmann <michel@kuhlmanns.info>
2013-07-16 11:38:20 +0200

    cosmetics in sagaTopo

--- src/sagaTopo.hs
+++ src/sagaTopo.hs
@@ -6,27 +6,26 @@ import           Math.Geometry.Saga.Cmd
 import           Math.Geometry.Saga.Data
 import           Math.Geometry.Saga.LUT
 import           Math.Geometry.Saga.Types
-import           Math.Geometry.Saga.Utils
 import           Prelude hiding (min,max)
 import           System.Console.CmdArgs
-import           System.Directory (renameFile)
 import           System.Environment (getArgs, withArgs)
 import           System.Posix.Temp (mkdtemp)
 
+_PROGRAM_NAME, _PROGRAM_VERSION, _PROGRAM_INFO, _PROGRAM_ABOUT, _COPYRIGHT :: String
 _PROGRAM_NAME    = "sagaTopo"
 _PROGRAM_VERSION = "0.0.0.1"
 _PROGRAM_INFO    = _PROGRAM_NAME ++ " version " ++ _PROGRAM_VERSION
 _COPYRIGHT       = "GPL licensed; written by Michel Kuhlmann 2013"
 _PROGRAM_ABOUT   = "Create create topographic maps out of gridded data"
-_PROGRAM_DETAILS = ["Program dispatches on input and output file extensions;"
-                   , "input-file: .las,.xyz.sgrd,.tif"]
 
+_COLOR_FILE :: FilePath
 _COLOR_FILE = "relief-colors.txt"
+
 main :: IO ()
 main = do
-    args <- getArgs
+    cArgs <- getArgs
     -- If the user did not specify any arguments, pretend as "--help" was given
-    opts <- (if null args then withArgs ["--help"] else id) (cmdArgs defaultOpts)
+    opts <- (if null cArgs then withArgs ["--help"] else id) (cmdArgs defaultOpts)
     r <- case (min opts, max opts) of
       (0.0, 0.0) -> sgrdToTopo (file opts) (output opts) Nothing Nothing
       _          -> sgrdToTopo (file opts) (output opts)
@@ -45,10 +44,10 @@ sgrdToTopo fIn fOut Nothing Nothing = do
           Nothing Nothing
           fIn fOut
 
-sgrdToTopo fIn fOut (Just min) (Just max) = do 
+sgrdToTopo fIn fOut (Just min') (Just max') = do 
   hsF <- mkdtemp "hillshade"
   doSaga $ gridHillShade fIn hsF
-  writeFile _COLOR_FILE (bgrColTable min max) -- color-lookup-table
+  writeFile _COLOR_FILE (bgrColTable min' max') -- color-lookup-table
   doSaga $ SagaCmd "libio_grid_image" "0" ("GRID", "FILE")
           (M.fromList [
               ("tifShade",("SHADE", hsF))
@@ -68,12 +67,11 @@ data Opt = Opt
 -- | Defaults for command-line options.
 defaultOpts :: Opt
 defaultOpts = Opt
-    {output = def &= help "output-file-name (supported extension: .tif,.png,.shp (contour-lines))"
+    {output = def &= help "output-tif"
     ,min    = def &= help "minimum elevation-value (optional)"
     ,max    = def &= help "maximum elevation-value (optional)"
     ,file        = def &= args &= typ "DEM-file.sgrd"
     } &=
     program _PROGRAM_NAME &=
     help _PROGRAM_ABOUT &=
-    summary (_PROGRAM_INFO ++ ", " ++ _COPYRIGHT) &=
-    details _PROGRAM_DETAILS
+    summary (_PROGRAM_INFO ++ ", " ++ _COPYRIGHT) 

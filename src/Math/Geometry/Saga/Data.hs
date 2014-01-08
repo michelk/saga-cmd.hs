40f27575d023604385802d27fe0f4c278cb99ce7
Michel Kuhlmann <michel@kuhlmanns.info>
2014-01-08 16:06:41 +0100

    removed 'lib' from library-names (saga v2.1)

--- src/Math/Geometry/Saga/Data.hs
+++ src/Math/Geometry/Saga/Data.hs
@@ -8,7 +8,7 @@ import qualified Data.Map as M
 sIoDB :: SagaIoCmdDB
 sIoDB = M.fromList [
   ("xyzGridToGrid", (
-      SagaCmd "libio_grid" "6" ("FILENAME","GRID") -- library, module, input-,output-parameters
+      SagaCmd "io_grid" "6" ("FILENAME","GRID") -- library, module, input-,output-parameters
               (M.fromList [     -- ^ parameters: sagaPipe-par, saga_cmd-par, default
                    ("cs",  ("CELLSIZE"  , "1"))
                   ,("sep", ("SEPARATOR" , "space"))
@@ -18,46 +18,49 @@ sIoDB = M.fromList [
       )
   )
   ,("lasToPtCld", (
-       SagaCmd "libio_shapes_las" "1" ("POINTS","FILE")
-       (M.fromList []) Nothing Nothing , ".pcl"))
+       SagaCmd "io_shapes_las" "1" ("FILE", "POINTS")
+       (M.fromList []) Nothing Nothing , ".spc"))
   ,("ptCldToGrid", (
-       SagaCmd "libpointcloud_tools" "4" ("POINTS","GRID")
-       (M.fromList []) Nothing Nothing ,".sgrd"))
+       SagaCmd "pointcloud_tools" "4" ("POINTS","GRID")
+       (M.fromList [
+                   ("cs",  ("CELLSIZE"  , "1"))
+                   ])
+       Nothing Nothing ,".sgrd"))
   ,("gridFillGaps", (
-       SagaCmd "libgrid_spline" "5" ("GRIDPOINTS","GRID_GRID")
+       SagaCmd "grid_spline" "5" ("GRIDPOINTS","GRID_GRID")
        (M.fromList [("grdFlT", ("TARGET", "1"))])
        (Just copyGrid) Nothing, "_filled.sgrd"))
   ,("gridHillshade", (
-       SagaCmd "libta_lighting" "0" ("ELEVATION","SHADE")
+       SagaCmd "ta_lighting" "0" ("ELEVATION","SHADE")
        (M.fromList []) Nothing Nothing , "_hillshade.sgrd"))
   ,("gridContour", (
-       SagaCmd "libshapes_grid" "5" ("INPUT","CONTOUR")
+       SagaCmd "shapes_grid" "5" ("INPUT","CONTOUR")
        (M.fromList [
             ("min" , ("ZMIN"  , "0"))
            ,("max" , ("ZMAX" , "10000"))
            ,("d",    ("ZSTEP" , "1"))
            ]) Nothing Nothing, "_contour.sgrd"))
   ,("gridPolyClip", (
-       SagaCmd "libshapes_grid" "7" ("INPUT","OUTPUT")
+       SagaCmd "shapes_grid" "7" ("INPUT","OUTPUT")
        (M.fromList [
            ("poly" , ("POLYGONS"  , ""))
            ]) Nothing Nothing, "_polyClip.sgrd"))
   ,("gridTifHillshade", (
-       SagaCmd "libio_grid_image" "0" ("GRID","FILE")
+       SagaCmd "io_grid_image" "0" ("GRID","FILE")
        (M.fromList [
            ("col" ,  ("COL_PALETTE" , "2"))
           ,("colRev",("COL_REVERT"  , ""))
            ]) Nothing Nothing, ".tif"))
   ,("gridTifGdal", (
-       SagaCmd "libio_gdal" "2" ("GRIDS","FILE")
+       SagaCmd "io_gdal" "2" ("GRIDS","FILE")
        (M.fromList [
            ]) Nothing Nothing, ".tif"))
   ,("gridXyz", (
-         SagaCmd "libio_grid" "5" ("GRIDS","FILENAME")
+         SagaCmd "io_grid" "5" ("GRIDS","FILENAME")
          (M.fromList [
              ]) Nothing Nothing, ".xyz"))
   ,("polyDissolve", (
-         SagaCmd "libshapes_polygons" "5" ("POLYGONS","DISSOLVED")
+         SagaCmd "shapes_polygons" "5" ("POLYGONS","DISSOLVED")
          (M.fromList [
            ("method" ,("DISSOLVE" , "0")) -- 0: polygons with same attribute value
                                          -- 1: all polygons
@@ -69,13 +72,13 @@ sIoDB = M.fromList [
          ])
          Nothing Nothing, "_disollved.shp"))
   ,("gridSlope", (
-         SagaCmd "libta_morphometry" "0" ("ELEVATION","SLOPE")
+         SagaCmd "ta_morphometry" "0" ("ELEVATION","SLOPE")
          (M.fromList [
             ("aspect" ,("ASPECT" , "aspect"))
          ])
          Nothing Nothing, "_slope.sgrd"))
   ,("gridClassifyFlat", (
-         SagaCmd "libgrid_tools" "15" ("INPUT","RESULT")
+         SagaCmd "grid_tools" "15" ("INPUT","RESULT")
          (M.fromList [
             ("method" ,("METHOD" , "2")) -- 1: range
                                          -- 2: simple table
@@ -85,7 +88,7 @@ sIoDB = M.fromList [
          (Just (\_ _ -> removeFile "reclassify.txt"))
                 , "_reclassified.sgrd"))
   ,("gridClassToPoly", (
-         SagaCmd "libshapes_grid" "6" ("GRID","POLYGONS")
+         SagaCmd "shapes_grid" "6" ("GRID","POLYGONS")
          (M.fromList [
            ("id",    ("CLASS_ID" , "1")) -- class identifier
           ,("all",   ("CLASS_ALL" , "0")) -- 0: one single class specified by class identifier

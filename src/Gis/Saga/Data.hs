6b09f86a2152bff6cac10a8127ece038fa29fd0c
Michel Kuhlmann <michel.kuhlmann@tkconsult.ch>
2020-02-07 11:48:35 +0100

    use saga 7.3

--- src/Gis/Saga/Data.hs
+++ src/Gis/Saga/Data.hs
@@ -97,8 +97,8 @@ sIoDB = M.fromList [
   ,("gridXyz", (
          SagaCmd "io_grid" "5" ("GRIDS","FILENAME")
          (M.fromList [
-           ("header",("CAPTION"  , "0"))
-          ,("na",("EX_NODATA"  , "1"))
+           ("header",("HEADER"  , "0"))
+          ,("na",("NODATA"  , "0"))
              ]) Nothing Nothing, ".xyz"))
   ,("polyDissolve", (
          SagaCmd "shapes_polygons" "5" ("POLYGONS","DISSOLVED")
